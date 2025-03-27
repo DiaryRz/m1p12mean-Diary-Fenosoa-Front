62a749727bf6f0255c08b5985284180afbb066ac
FenosoaTiavina <fenosoa.t.r@gmail.com>
2025-03-27 15:02:14 +0300

    Fixes

--- src/app/pages/manager/employee/employee-list.component.ts
+++ src/app/pages/manager/employee/employee-list.component.ts
@@ -14,6 +14,7 @@ import { CreateEmployeeComponent } from './items/create/create-employee.componen
   templateUrl: './employee-list.component.html',
 })
 export class EmployeeListComponent implements OnInit {
+
   employees: EmployeeItem[];
   filteredEmployees: EmployeeItem[] = [];
 
@@ -44,7 +45,6 @@ export class EmployeeListComponent implements OnInit {
     this.filterRole = '';
     this.applyFilters();
   }
-
   ngOnInit(){
     this.isFetching = true;
     this.userService.getAllEmployee().subscribe((val) => {
--- src/app/pages/manager/services/services/create-item/create-item.component.ts
+++ src/app/pages/manager/services/services/create-item/create-item.component.ts
@@ -17,15 +17,27 @@ import { ServicesService }            from 'src/app/services/services.service';
   imports: [MaterialModule, FormsModule, ReactiveFormsModule ],
   templateUrl: './create-item.component.html',
 })
-export class CreateItemComponent {
+export class CreateItemComponent{
   fb = inject(FormBuilder);
   serviceService = inject(ServicesService);
   @Output() newItemAdded = new EventEmitter<void>();
 
   _form = this.fb.nonNullable.group({
     service_name  : [ '' , Validators.required],
-    unit_price    : [ Validators.required],
-    time_needed   : [ Validators.required],
+    unit_price    : [
+      [
+        Validators.required,
+        Validators.min(0),
+        Validators.pattern(/^[0-9]+$/)
+      ]
+    ],
+    time_needed   : [
+      [
+        Validators.required,
+        Validators.min(0),
+        Validators.pattern(/^[0-9]+$/)
+      ]
+    ],
     need_mult     : [ false, Validators.required],
     ressources    : [ Validators.required],
   });
@@ -33,6 +45,12 @@ export class CreateItemComponent {
   get form(): FormGroup {
     return this._form;
   }
+
+  numericOnly(event: KeyboardEvent): boolean {
+    const charCode = event.which ? event.which : event.keyCode;
+    return !(charCode > 31 && (charCode < 48 || charCode > 57));
+  }
+
 
   submit(){
     let value = this.form.getRawValue();
